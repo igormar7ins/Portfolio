844906248ebe227774e1783bfd31d8d6491e648c
igormar7ins <igormartins135@gmail.com>
2025-11-13 00:47:08 -0300

    Hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { HeroSectionComponent } from "./features/components/hero-section/hero-section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
