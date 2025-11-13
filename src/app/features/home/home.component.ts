2d6593cdcc19a27496b9d2d8cc4035deeeca8758
Eduardo <edununes726@gmail.com>
2025-11-13 02:08:57 -0300

    ✨feat(home): add background image styles and optimize image loading.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
