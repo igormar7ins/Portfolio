617d68126023757abed3fcd7a8c20cd0eedb3302
Eduardo <edununes726@gmail.com>
2025-11-18 23:45:12 -0300

    Revert "Merge branch 'develop-edu' into develop"
    This reverts commit 4a4cbcec10e85d511ffe427f6ce86be36b5e3d6f, reversing
    changes made to e3e9e835cb5a23aa764a10713c7174ec2d4464d2.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonDirective } from '../../directives/button.directive';
 
 @Component({
@@ -6,7 +6,6 @@ import { ButtonDirective } from '../../directives/button.directive';
   imports: [ButtonDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
 
--- src/app/shared/directives/button.directive.ts
+++ src/app/shared/directives/button.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, InputSignal, Renderer2, input } from '@angular/core';
+import { Directive, ElementRef, InputSignal, Renderer2, effect, input } from '@angular/core';
 
 @Directive({
   selector: '[appButton]',
